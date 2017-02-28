3b805b238bff9362ab216d3303021dc1e09966df
Lauren <lauren.l.niu@gmail.com>
2017-02-28 15:26:01 -0500

    yes

--- simulation/go.sh
+++ simulation/go.sh
@@ -61,7 +61,7 @@ mv jobOptions2.py jobOptions.py
 
 # run the code
 now=$(date +%Y-%m-%d-%Hh%Mm%Ss)
-athena.py sim.n.dump.jobOptions.py 2>&1 | tee log_${now}.txt
+athena.py jobOptions.py 2>&1 | tee log_${now}.txt
 
 # fuck athena
 rm -f AtDSFMTGenSvc.out PoolFileCatalog.xml PoolFileCatalog.xml.BAK SimParams.db eventLoopHeartBeat.txt hits.pool.root
